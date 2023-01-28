0c07c4f4c3d9f63b4e01d12216a544ad393d1614
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2023-01-28 15:30:24 +0500

    Login page view

--- WaterControl/App.xaml.cs
+++ WaterControl/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WaterControl.Windows;
 
 namespace WaterControl
 {
@@ -13,5 +14,10 @@ namespace WaterControl
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+        }
     }
 }
