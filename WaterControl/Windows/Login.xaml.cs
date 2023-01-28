d71a3fa6e96e3bc5fbfd2745ef56341a4b56e585
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2023-01-28 15:55:10 +0500

    LoginWindow sign in logic

--- WaterControl/Windows/Login.xaml.cs
+++ WaterControl/Windows/Login.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,11 +19,97 @@ namespace WaterControl.Windows
     /// <summary>
     /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class Login : Window
+    public partial class Login : Window, INotifyPropertyChanged
     {
         public Login()
         {
             InitializeComponent();
         }
+        private bool isJustStarted = true;
+        private bool isLoggedIn = false;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool IsLoggedIn
+        {
+            get => isLoggedIn;
+            set
+            {
+                isLoggedIn = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool IsJustStarted
+        {
+            get => isJustStarted;
+            set
+            {
+                isJustStarted = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private async Task<bool> validateCreds()
+        {
+            if(UserName.Text == "Admin" && UserPassword.Password  == "123456")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private async void openCB(object sender, MaterialDesignThemes.Wpf.DialogOpenedEventArgs eventArgs)
+        {
+            try
+            {
+                if (await validateCreds())
+                {
+                    eventArgs.Session.Close(true);
+                }
+                else
+                {
+                    eventArgs.Session.Close(false);
+                }
+            }
+            catch (Exception)
+            {
+                //throw
+            }
+        }
+
+        private async void closeCB(object sender, MaterialDesignThemes.Wpf.DialogClosingEventArgs eventArgs)
+        {
+            if (eventArgs.Parameter != null)
+            {
+                if ((bool)eventArgs.Parameter)
+                {
+                    //Login Success
+                    IsLoggedIn = true;
+
+                    IsJustStarted = false;
+
+                    MainWindow main = new MainWindow();
+                    main.Show();
+                    this.Close();
+                }
+                else
+                {
+                    //Login Fail
+                    IsLoggedIn = false;
+
+                    IsJustStarted = false;
+                }
+            }
+        }
     }
 }
+
